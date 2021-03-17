46babe4265a6b06000295db6a92a5acc4c8c17f5
Psp-pixel <53408419+Psp-pixel@users.noreply.github.com>
2021-03-17 20:24:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 function compute()
-{
-    var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("super").value;
-    var years = document.getElementById("years").value;
-    var interest = parseFloat(principal) * parseFloat(years) * parseFloat(rate) / 100;
-    var d = new Date();
-    var n = d.getFullYear();
-    var future_years = parseInt(n) + parseInt(years);
-    document.getElementById("result").innerHTML = "If you deposit " + principal + ", <br>" 
-    + "at an interest rate of " + rate + ". <br>"
-    + "You will receive an amount of " + interest + ", <br>" 
-    + "in the year " + future_years + "<br>";
+{   //getting input value into the function
+    var p = document.getElementById("principal").value;
+    var r = document.getElementById("rate").value;
+    var y = document.getElementById("years").value;
+  
+// Validation of positive number for "principal" input
+    if (p <= 0){window.alert("Enter a positive number")
+document.getElementById("principal").focus();}
+   else {         
+//calcuation of interest
+    var interest = p * r * y / 100;
+
+//adding the current year with the year input
+    var yearNow = new Date().getFullYear();
+    var yearsFromNow = (+yearNow) + (+y);
+//display result from calculation
+var result = document.getElementById("result");
+    result.innerHTML = 
+        '<div>' +
+            'If you deposit <span class="number" id="principalResult\">' + p + '</span>,<br/>' +
+            'at an interest rate of <span class="number" id="rateResult">' + r + '%</span>.<br/>' +
+            'You will receive an amount of <span class="number" id="interestResult">' + interest + '</span>,<br/>' +
+            'in the year <span class="number" id="futureYearResult">' + yearsFromNow + '</span>.<br/>' +
+        '</div>'
+}  
 }
-function show_value(x)
-{
-    document.getElementById("slider_value").innerHTML=x;
-}
+
+function slider()
+{ //display the value from slider
+    var sv = document.getElementById("rate").value;
+document.getElementById("slider_value").innerHTML = sv + "%";}
