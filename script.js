2a67ae412e57cc1507628501ec95f38bbbef121e
Psp-pixel <53408419+Psp-pixel@users.noreply.github.com>
2021-03-17 19:53:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("super").value;
+    var years = document.getElementById("years").value;
+    var interest = parseFloat(principal) * parseFloat(years) * parseFloat(rate) / 100;
+    var d = new Date();
+    var n = d.getFullYear();
+    var future_years = parseInt(n) + parseInt(years);
+    document.getElementById("result").innerHTML = "If you deposit " + principal + ", <br>" 
+    + "at an interest rate of " + rate + ". <br>"
+    + "You will receive an amount of " + interest + ", <br>" 
+    + "in the year " + future_years + "<br>";
 }
-        
+function show_value(x)
+{
+    document.getElementById("slider_value").innerHTML=x;
+}
